199d06cf390316b0c839feb5ab513cd176aca38a
Brandon <brandontat6@gmail.com>
2023-12-10 11:46:17 -0800

    WIP day 4

--- day4.go
+++ day4.go
@@ -6,8 +6,25 @@ import (
 	"strings"
 )
 
+func parseNumbers(line string) ([]string, []string) {
+	fmt.Println(line)
+	splitLine := strings.Split(line, "|")
+	winningNumberStr := strings.Split(splitLine[0], ":")
+	winningNumberArr := strings.Split(winningNumberStr[1], " ")
+
+	actualNumberArr := strings.Split(splitLine[1], " ")
+
+	fmt.Println(winningNumberArr, actualNumberArr)
+
+	return nil, nil
+}
+
 func main() {
 	fileBytes, _ := os.ReadFile("sample.txt")
-	linesArray := strings.Split(string(fileBytes), "\n")
-	fmt.Println(linesArray)
+	trimmedFile := strings.TrimSpace(string(fileBytes))
+	linesArray := strings.Split(trimmedFile, "\n")
+
+	for _, line := range linesArray {
+		parseNumbers(line)
+	}
 }

acc0324d7d6650e648ffcca1163b0b7b3895e157
Brandon Tat <brandontat6@gmail.com>
2025-12-24 15:09:18 -0800

    WIP day 2

--- go/2025/day02/main.go
+++ go/2025/day02/main.go
@@ -4,15 +4,46 @@ import (
 	"aoc/utils"
 	"fmt"
 	"os"
+	"strings"
 )
 
+type Range struct {
+	Start int
+	End   int
+}
+
+func Parse(line string) []Range {
+	var ranges []Range
+
+	for part := range strings.SplitSeq(line, ",") {
+		numbers := strings.Split(part, "-")
+		if len(numbers) == 2 {
+			start := strings.TrimSpace(numbers[0])
+			end := strings.TrimSpace(numbers[1])
+
+			var rangeObj Range
+			fmt.Sscanf(start, "%d", &rangeObj.Start)
+			fmt.Sscanf(end, "%d", &rangeObj.End)
+
+			ranges = append(ranges, rangeObj)
+		}
+	}
+
+	return ranges
+}
+
 func main() {
-	lines, err := utils.ReadInputLines(2025, 2, false)
+	lines, err := utils.ReadInputLines(2025, 2, true)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error reading input: %v\n", err)
 		os.Exit(1)
 	}
 
+	productRanges := Parse(lines[0])
+
+	for _, r := range productRanges {
+		fmt.Println(r)
+	}
+
 	fmt.Println("Starting day 2")
-	fmt.Printf("Read %d lines\n", len(lines))
 }
